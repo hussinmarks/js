5e007e86c177095d57089558382049568dfd04a2
hussinmarks <hussinmarks6@gmail.com>
2023-12-04 17:58:05 -0500

    monday class

--- loops/while_loops.js
+++ loops/while_loops.js
@@ -108,9 +108,20 @@ villain.hp <= 0
   ? console.log(`Spider-Man captures ${villain.baddie}`)
   : console.log(`${villain.baddie} has taken down Spider-Man`);
 
+
+
+
+
+
+
+
+
+
+
+
 // fizz buzz
 
-  for (var i = 1; i < 101; i++) {
+  for (let i = 1; i < 101; i++) {
     if (i % 15 == 0) console.log("FizzBuzz");
     else if (i % 3 == 0) console.log("Fizz");
     else if (i % 5 == 0) console.log("Buzz");
